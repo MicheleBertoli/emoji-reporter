0ee4d32216567b2ed306e441f1a85a19821288fd
Michele Bertoli <michele@berto.li>
2016-06-15 20:55:47 +0100

    inherit from base

--- src/index.js
+++ src/index.js
@@ -1,52 +1,25 @@
-import colors from 'colors'
-import prettyMs from 'pretty-ms'
+import { inherits } from 'util'
+import base from 'mocha/lib/reporters/base'
 
-export default runner => {
-  const stats = {
-    start: null,
-    passes: 0,
-    pending: 0,
-    failures: 0,
-  }
-
-  runner.on('start', () => {
-    stats.start = new Date()
-  })
+export default function emoji(runner) {
+  base.call(this, runner)
 
   runner.on('pass', () => {
-    stats.passes++
     process.stdout.write('😻 ')
   })
 
   runner.on('pending', () => {
-    stats.pending++
     process.stdout.write('🙀 ')
   })
 
   runner.on('fail', () => {
-    stats.failures++
     process.stdout.write('😿 ')
   })
 
   runner.on('end', () => {
-    process.stdout.write('\n')
-
     console.log()
-
-    const duration = new Date() - stats.start
-    console.log(
-      `${stats.passes} passing`.green,
-      `(${prettyMs(duration)})`.grey
-    )
-
-    if (stats.pending) {
-      console.log(`${stats.pending} pending`.cyan)
-    }
-
-    if (stats.failures) {
-      console.log(`${stats.failures} failing`.red)
-    }
-
-    console.log()
+    this.epilogue()
   })
 }
+
+inherits(emoji, base)
