0ec76362396e4d692da185ce8fdbe6f4d882df0f
Michele Bertoli <michele@berto.li>
2016-06-15 11:09:54 +0100

    better logging

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
+import colors from 'colors'
+import prettyMs from 'pretty-ms'
+
 export default runner => {
-  let passes = 0
-  let failures = 0
+  const stats = {
+    start: null,
+    passes: 0,
+    pending: 0,
+    failures: 0,
+  }
+
+  runner.on('start', () => {
+    stats.start = new Date()
+  })
 
   runner.on('pass', () => {
-    passes++
+    stats.passes++
     process.stdout.write('😻 ')
   })
 
+  runner.on('pending', () => {
+    stats.pending++
+    process.stdout.write('🙀 ')
+  })
+
   runner.on('fail', () => {
-    failures++
+    stats.failures++
     process.stdout.write('😿 ')
   })
 
   runner.on('end', () => {
     process.stdout.write('\n')
-    console.log('%d/%d', passes, passes + failures)
+
+    console.log()
+
+    const duration = new Date() - stats.start
+    console.log(
+      `${stats.passes} passing`.green,
+      `(${prettyMs(duration)})`.grey
+    )
+
+    if (stats.pending) {
+      console.log(`${stats.pending} pending`.cyan)
+    }
+
+    if (stats.failures) {
+      console.log(`${stats.failures} failing`.red)
+    }
+
+    console.log()
   })
 }
